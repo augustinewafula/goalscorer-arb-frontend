392e9e2c316680ff0f7f3ff33aa3a98aaa952658
AugustineTreezy <augustinetreezy@outlook.com>
2019-07-20 12:39:06 +0300

    update api base url

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['x-access-token'] = localStorage.token
 // axios.defaults.baseURL = window.location.origin + '/backend/public/api'
 if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'http://167.99.166.104:5000'
+  axios.defaults.baseURL = 'https://goalscorer.riwaa.co.ke:5000'
 } else {
   axios.defaults.baseURL = 'http://127.0.0.1:5000'
 }
