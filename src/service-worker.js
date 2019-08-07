7b77852eeadc316fd07f7e56cc9bab1dceeeaaa9
AugustineTreezy <augustinetreezy@outlook.com>
2019-08-07 17:42:01 +0300

    update service worker version just for a test

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,7 +3,7 @@ if (workbox) {
     workbox.core.setCacheNameDetails({ prefix: 'd4' })
 
     //Change this value every time before you build
-    const LATEST_VERSION = 'v1.1'
+    const LATEST_VERSION = 'v1.2'
     self.addEventListener('activate', (event) => {
       console.log(`%c ${LATEST_VERSION} `, 'background: #ddd; color: #0000ff')
       if (caches) {
